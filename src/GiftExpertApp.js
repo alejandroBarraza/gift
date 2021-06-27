01809590a8958c20072567f3444a782f4ee38ce1
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-06-27 00:42:44 -0400

    starting files

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GiftExpertApp = () => {
+    return (
+        <div>
+            <h2>Gift Expert app</h2>
+            <hr />    
+        </div>
+    )
+}
