fcdd34d8f644b57aae8a0c0c732399550490f2c6
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-06-27 18:04:04 -0400

    add list with map

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 export const GiftExpertApp = () => {
+    const dataArray = ['dragon ball', 'naruto', 'HunterxHunter'];
+    const [categories, setcategories] = useState(dataArray);
+
+    //add new elemet to dataArray
+    const handleAdd = () => {
+        setcategories((cat) => [...cat, 'sao']);
+    };
+
     return (
         <div>
             <h2>Gift Expert app</h2>
-            <hr />    
+            <hr />
+            <button onClick={handleAdd}>addElement</button>
+            <ol className='list'>
+                {categories.map((category) => {
+                    return <li key={category}>{category}</li>;
+                })}
+            </ol>
         </div>
-    )
-}
+    );
+};
