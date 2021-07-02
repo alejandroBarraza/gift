d6663e864ee21a919e605d830ff778be8991369f
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-07-02 00:07:03 -0400

    useEffect for fetch data gift

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,13 +1,21 @@
-import React from 'react';
+
+import React,{useState,useEffect} from 'react';
+
 
 export const GifGrid = ({ category }) => {
-    // const API_KEY = 'NteEFqYmRZK13pkumKPqO7RpeXscN39l';
+    
+    
+    useEffect(() => {
+        getGift()
+    }, [])
+    const [imgs, setimgs] = useState([""])
+
+
 
     const getGift = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?api_key=NteEFqYmRZK13pkumKPqO7RpeXscN39l&q=naruto&limit=10';
         const response = await fetch(url);
-        const { data } = await response.json();
-        // console.log(data);
+        const {data} = await response.json();
         const gifs = data.map((gif) => {
             return {
                 id: gif.id,
@@ -15,13 +23,24 @@ export const GifGrid = ({ category }) => {
                 url: gif.images?.downsized_medium.url,
             };
         });
-        console.log(gifs);
+        setimgs(gifs)
     };
-    getGift();
+    
 
     return (
         <div>
             <h2>{category}</h2>
+            <ol>
+                {
+                    imgs.map(({id,title,url}) => {
+                        return [
+                            <li key={id}>{title} </li>,
+                            <img src={url} alt="#" />
+                        ]
+                    })
+                }
+            </ol>
+            
         </div>
     );
 };
