3abb13d5ac0468bf00172ec7674c3f752ae20cad
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-07-03 01:02:27 -0400

    move fetch to helpers

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,43 +1,30 @@
 
 import React,{useState,useEffect} from 'react';
+import { getGift } from '../helpers/getGifts';
 import { GiftGridItem } from './GiftGridItem';
+
 
 
 export const GifGrid = ({ category }) => {
 
-    const API_KEY = 'NteEFqYmRZK13pkumKPqO7RpeXscN39l'
-    
     const [imgs, setimgs] = useState([])
     
     useEffect(() => {
         getGift(category)
-    }, [])
-
-
-
-    const getGift = async (imgToFetch) => {
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${imgToFetch}&limit=10`;
-        const response = await fetch(url);
-        const {data} = await response.json();
-        const gifs = data.map((gif) => {
-            return {
-                id: gif.id,
-                title: gif.title,
-                url: gif.images?.downsized_medium.url,
-            };
-        });
-        setimgs(gifs)
-    };
+            .then((imgs)=> setimgs(imgs))
+            
+            
+    }, [category])
     
 
     return (
-
-        
-        <div>
+        <>
             <h2>busqueda ralizada : {category}</h2>
-            {
-                imgs.map( (img) => <GiftGridItem key={img.id} {...img} />)
-            }   
-        </div>
+            <div className='card-container'>
+                {
+                    imgs.map( (img) => <GiftGridItem key={img.id} {...img} />)
+                }   
+            </div>
+        </>
     );
 };
