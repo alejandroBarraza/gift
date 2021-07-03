470cee97cbe308cfd9e914cdfdf43907a4d6338c
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-07-03 01:54:59 -0400

    style and new refactor giftgrid

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,25 +1,21 @@
 
-import React,{useState,useEffect} from 'react';
-import { getGift } from '../helpers/getGifts';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+
 import { GiftGridItem } from './GiftGridItem';
 
 
 
 export const GifGrid = ({ category }) => {
 
-    const [imgs, setimgs] = useState([])
-    
-    useEffect(() => {
-        getGift(category)
-            .then((imgs)=> setimgs(imgs))
-            
-            
-    }, [category])
+
+    const {data:imgs} = useFetchGifs(category)
     
 
     return (
         <>
             <h2>busqueda ralizada : {category}</h2>
+           
             <div className='card-container'>
                 {
                     imgs.map( (img) => <GiftGridItem key={img.id} {...img} />)
