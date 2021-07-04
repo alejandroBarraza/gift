f159d0c607475d637ebb52cea0ea7887b1355689
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-07-03 20:46:51 -0400

    add animation to card

--- src/components/GiftGridItem.js
+++ src/components/GiftGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GiftGridItem = ({url,title}) => {
     
     return (
-        <div className= 'card' >
+        <div className= 'card animate__animated animate__bounce' >
             <p>{title}</p>
             <img src={url} alt={title} />
         </div>
