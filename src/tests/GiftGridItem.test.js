8890bfe983cb77bad460a6a1b7365144415e42f3
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-07-17 01:07:18 -0400

    test getGrifItem

--- src/tests/GiftGridItem.test.js
+++ src/tests/GiftGridItem.test.js
@@ -3,7 +3,7 @@ import { GiftGridItem } from '../components/GiftGridItem';
 import '../setupTests';
 import { shallow } from 'enzyme';
 
-describe('testing gridGridItem in enzyme', () => {
+describe('testing <GiftGridItem />', () => {
     let wrapper;
     const url = 'https://localhost:3000';
     const title = 'naruto';
@@ -14,4 +14,23 @@ describe('testing gridGridItem in enzyme', () => {
     test('should match snapshot', () => {
         expect(wrapper).toMatchSnapshot();
     });
+    test(' <p /> should contain {title} given as prop', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+
+        
+    })
+    test('<img /> should contai a {url} and {title} given as prop', () => {
+        const img = wrapper.find('img');
+        expect(img.props()).toEqual({src:url, alt:title})
+    })
+
+    test('<div> from GifGridItem should contain classes given', () => {
+        const div = wrapper.find('div');
+        expect(div.hasClass('card glassmorphismo')).toBe(true);
+        
+    })
+    
+    
+    
 });
