c764d9b5871e9e5e99da947eb9eb3bd765faf43f
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-07-23 00:14:04 -0400

    giftGrid test

---
+++ src/tests/GifGrid.test.js
@@ -0,0 +1,39 @@
+import '@wojtekmaj/enzyme-adapter-react-17';
+import '../setupTests';
+import { shallow } from 'enzyme';
+import { GifGrid } from '../components/GifGrid';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+jest.mock('../hooks/useFetchGifs');
+
+describe('testing <GifGrid /> component', () => {
+    const category = 'naruto';
+    let wrapper;
+    beforeEach(() => {
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true,
+        });
+        wrapper = shallow(<GifGrid category={category} />);
+    });
+    afterEach(() => {
+        wrapper.unmount();
+    });
+    test('should match <GifGrid category={cateogory}/>  with snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+    test('should load item after use fetch in customHook UseFetchGifts', () => {
+        const imgs = [
+            {
+                id: 13123,
+                title: category,
+                url: 'htttps://www.gift/naruto.png',
+            },
+        ];
+        useFetchGifs.mockReturnValue({
+            data: imgs,
+            loading: false,
+        });
+        wrapper = shallow(<GifGrid category={category} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
